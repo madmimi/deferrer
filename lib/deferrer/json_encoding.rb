eace28fbd8c369801c312e99bc4c1c888e8c8c2d
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2014-07-24 11:11:23 +0200

    Remove DecodeException

--- lib/deferrer/json_encoding.rb
+++ lib/deferrer/json_encoding.rb
@@ -3,18 +3,12 @@ require 'multi_json'
 module Deferrer
   module JsonEncoding
 
-    class DecodeException < StandardError; end
-
     def encode(item)
       MultiJson.dump(item)
     end
 
     def decode(item)
-      begin
-        MultiJson.load(item)
-      rescue ::MultiJson::DecodeError => e
-        raise DecodeException, e.message, e.backtrace
-      end
+      MultiJson.load(item)
     end
   end
 end
